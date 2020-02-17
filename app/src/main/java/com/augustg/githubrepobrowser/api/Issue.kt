7fd005d99367ad5b997f15f43a0cce7e3cd35d9b
August Gruneisen <august.gruneisen@smartearth.net>
2020-02-17 13:34:05 -0800

    Serialization for child nodes

--- app/src/main/java/com/augustg/githubrepobrowser/api/Issue.kt
+++ app/src/main/java/com/augustg/githubrepobrowser/api/Issue.kt
@@ -3,6 +3,9 @@ package com.augustg.githubrepobrowser.api
 import com.google.gson.annotations.SerializedName
 
 data class Issue(@SerializedName("title") val title: String,
+                 @SerializedName("body") val body: String,
                  @SerializedName("number") val number: Int,
-                 @SerializedName("state") val account: String,
-                 @SerializedName("comments") val commentCount: Int)
+                 @SerializedName("comments") val commentCount: Int,
+                 @SerializedName("user") val user: User)
+
+data class User(@SerializedName("login") val login: String)
